da675480d6c2416e262ca4985155c52bf00783e0
rjaugusto <rjaugustobe@yahoo.com.br>
2020-07-30 19:19:11 -0300

    Adição das funções de state

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,91 @@
-import React from 'react';
+import React,{useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import{Link} from 'react-router-dom'
 
 function Categoria(){
-    return(<PageDefault>
 
-       <h1>Cadastrar Categoria</h1> 
+    const categorias = []
 
-       <Link as="Button" to="/">Ir para Home</Link>
+    const valoresIniciais = {
+      nome:'Categoria Inicial',
+      descricao:'Descrção Inicial',
+      cor:'#000'
+    }
 
+    let [values,setValues] = useState(valoresIniciais); 
 
-    </PageDefault>)
+    return(
+        <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome} {values.descricao} {values.cor}</h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvent){
+          infosDoEvent.preventDefault()
+        //  setCategorias([...categorias,values]);
+      } } >
+
+        <div>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+              value={values.nome} 
+              onChange={
+                  (infosDoEvento) => {
+                    values.nome = infosDoEvento.target.value
+                    
+                  }
+              }
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Descrição:
+            <textarea
+      
+              value={values.descricao} 
+              onChange={
+                  (infosDoEvento) => {
+                    //setDescricao(infosDoEvento.target.value)
+                  }
+              }
+            />
+          </label>
+        </div>  
+
+        <div>  
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor} 
+              onChange={
+                  (infosDoEvento) => {
+                  //  setCor(infosDoEvento.target.value)
+                  }
+              }
+            />
+          </label>
+        </div>
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+        <ul>
+        {categorias.map(
+            (categoria,indice) =>{
+                return <li key={`${categoria} ${indice}`}> {categoria}</li>
+            }
+        )}
+        </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+        )
         
     
 }
